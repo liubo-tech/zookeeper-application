b48d2a2307115d9a6205398c8bf00ed8f54a0155
liubo <liubo@echinacoop.com>
2018-07-02 15:27:49 +0800

    zookeeper-barrier

--- zookeeper-barrier/src/main/java/com/example/zookeeper/barrier/Application.java
+++ zookeeper-barrier/src/main/java/com/example/zookeeper/barrier/Application.java
@@ -3,27 +3,15 @@ package com.example.zookeeper.barrier;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import org.apache.log4j.LogManager;
 import org.slf4j.LoggerFactory;
-import org.slf4j.helpers.SubstituteLoggerFactory;
 
-import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Application {
     public static void main(String[] args) {
-
-        //1.logback
-        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        //获取应用中的所有logger实例
-        List<Logger> loggerList = loggerContext.getLoggerList();
-
-        //遍历更改每个logger实例的级别,可以通过http请求传递参数进行动态配置
-        for (ch.qos.logback.classic.Logger logger:loggerList){
-            logger.setLevel(Level.toLevel("ERROR"));
-        }
+        setLog();
 
         ExecutorService es = Executors.newFixedThreadPool(5);
 
@@ -33,5 +21,17 @@ public class Application {
         es.shutdown();
 
         System.out.println("主线程完成！");
+    }
+
+    public static void setLog(){
+        //1.logback
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        //获取应用中的所有logger实例
+        List<Logger> loggerList = loggerContext.getLoggerList();
+
+        //遍历更改每个logger实例的级别,可以通过http请求传递参数进行动态配置
+        for (ch.qos.logback.classic.Logger logger:loggerList){
+            logger.setLevel(Level.toLevel("ERROR"));
+        }
     }
 }
